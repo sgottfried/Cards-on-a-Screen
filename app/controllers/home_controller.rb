40d091f977cf8b00b175cff84acaea82ae4b6345
Daniel R <daniel@Daniel-Rs-iMac.local>
2011-09-10 01:15:21 -0400

    added pg to gemfile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,11 @@ class HomeController < ApplicationController
   end
 
   def cards_moved
+    card = Card.find_by_id(params[:id])
+    card.left = params[:left]
+    card.top = params[:top]
+    card.save!
+    
     render :nothing => true
   end
 end
