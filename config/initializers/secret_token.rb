0c2af34ce7a8fb6967f863183ee964ed4bff458f
Daniel R <daniel@Daniel-Rs-iMac.local>
2011-09-09 23:38:52 -0400

    empty rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardsOnAScreen::Application.config.secret_token = '68700ddf0e2e2ca4bac833afac9e97eeb8d4ec90222201d77df691843b17b3a0f78eb97c41d997a6e840f866c1f990c73ab4248cacfcde33cb9d45af542c3879'
